3c2a70fd4e8443d6a53d60e0a6dea9084e64a6d9
Julian Sanchez <91.julian.sanchez@gmail.com>
2024-11-07 12:01:59 -0500

    chore: add type annotation for mypy compliance

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+from typing import Dict
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/health")
-def read_health():
+def read_health() -> Dict[str, str]:
     return {"status": "ok"}
